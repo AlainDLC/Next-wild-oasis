b67aaa0d79a5256a94340c84ebe1fa4ea3b691b4
AlainDLC <jd_alain@hotmail.com>
2024-11-07 18:14:50 +0100

    nested navigation

--- oasis-website/app/layout.js
+++ oasis-website/app/layout.js
@@ -1,8 +1,7 @@
-import Logo from "@/app/_components/Logo";
-import Navigation from "@/app/_components/Navigation";
 import "@/app/_styles/globals.css";
 
 import { Josefin_Sans } from "next/font/google";
+import Header from "./_components/Header";
 
 const josefin = Josefin_Sans({ subsets: ["latin"], display: "swap" });
 
@@ -20,15 +19,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100  min-h-screen `}
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
       >
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
+        <Header />
 
-        <main>{children} </main>
-        <footer>&#169; VOPT536</footer>
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
+        </div>
       </body>
     </html>
   );
