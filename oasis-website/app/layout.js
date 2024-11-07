3dbb4811484932044f8a5826c275c6047e51f209
AlainDLC <jd_alain@hotmail.com>
2024-11-07 16:14:46 +0100

    implement font

--- oasis-website/app/layout.js
+++ oasis-website/app/layout.js
@@ -1,21 +1,34 @@
 import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
+import "@/app/_styles/globals.css";
+
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({ subsets: ["latin"], display: "swap" });
+
+console.log(josefin);
 
 export const metadata = {
-  title: "Wild Oasis",
+  title: {
+    template: "%s The Wild Oasis",
+    default: "Welcome / The Wild Oasis",
+  },
+  description: "Luxurious cabin hotel",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-100  min-h-screen `}
+      >
         <header>
           <Logo />
-          <Navigation />{" "}
+          <Navigation />
         </header>
 
         <main>{children} </main>
-        <footer>Copyright dlc</footer>
+        <footer>&#169; VOPT536</footer>
       </body>
     </html>
   );
