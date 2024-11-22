73ab188c06dadb37e5e594c1c350ba3ed2227bf4
AlainDLC <jd_alain@hotmail.com>
2024-11-22 19:27:53 +0100

    tested new hook

--- oasis-website/app/_components/UpdateProfileForm.js
+++ oasis-website/app/_components/UpdateProfileForm.js
@@ -1,18 +1,23 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 
-import React, { useState } from "react";
+import { useFormStatus } from "react-dom";
+import { updateGuest } from "../_lib/action";
 
-const UpdateProfileForm = ({ children }) => {
-  const [count, setCount] = useState();
+const UpdateProfileForm = ({ guest, children }) => {
+  const { fullName, email, nationality, nationalID, countryFlag } = guest;
 
-  const countryFlag = "pt.jpg";
-  const nationality = "portugal";
   return (
-    <form className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
+    <form
+      action={updateGuest}
+      className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col"
+    >
       <div className="space-y-2">
         <label>Full name</label>
         <input
           disabled
+          defaultValue={fullName}
+          name="fullName"
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
         />
       </div>
@@ -21,6 +26,8 @@ const UpdateProfileForm = ({ children }) => {
         <label>Email address</label>
         <input
           disabled
+          defaultValue={email}
+          name="email"
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
         />
       </div>
@@ -30,6 +37,7 @@ const UpdateProfileForm = ({ children }) => {
           <label htmlFor="nationality">Where are you from?</label>
           <img
             src={countryFlag}
+            name="countryFlag"
             alt="Country flag"
             className="h-5 rounded-sm object-cover"
           />
@@ -41,17 +49,29 @@ const UpdateProfileForm = ({ children }) => {
         <label htmlFor="nationalID">National ID number</label>
         <input
           name="nationalID"
+          defaultValue={nationalID}
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
         />
       </div>
 
       <div className="flex justify-end items-center gap-6">
-        <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-          Update profile
-        </button>
+        <Button />
       </div>
     </form>
   );
 };
+
+function Button() {
+  const { pending } = useFormStatus();
+
+  return (
+    <button
+      disabled={pending}
+      className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
+    >
+      {pending ? "Updating..." : "Update profile"}
+    </button>
+  );
+}
 
 export default UpdateProfileForm;
