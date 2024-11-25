15207484d561b4cf0db069190a4cee3c17db37b3
AlainDLC <jd_alain@hotmail.com>
2024-11-25 12:03:55 +0100

    implement edit featurs

--- oasis-website/app/_components/UpdateProfileForm.js
+++ oasis-website/app/_components/UpdateProfileForm.js
@@ -3,6 +3,7 @@
 
 import { useFormStatus } from "react-dom";
 import { updateGuest } from "../_lib/action";
+import SubmitButton from "./SubmitButton";
 
 const UpdateProfileForm = ({ guest, children }) => {
   const { fullName, email, nationality, nationalID, countryFlag } = guest;
@@ -55,23 +56,10 @@ const UpdateProfileForm = ({ guest, children }) => {
       </div>
 
       <div className="flex justify-end items-center gap-6">
-        <Button />
+        <SubmitButton pendingLabel="Updating... ">Update profile</SubmitButton>
       </div>
     </form>
   );
 };
-
-function Button() {
-  const { pending } = useFormStatus();
-
-  return (
-    <button
-      disabled={pending}
-      className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
-    >
-      {pending ? "Updating..." : "Update profile"}
-    </button>
-  );
-}
 
 export default UpdateProfileForm;
